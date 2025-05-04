c55acebe99e3978e74bc4d22f0b30c9a5078f51a
Lapis0x0 <szy2089931398@gmail.com>
2025-05-04 09:05:34 +0200

    更新setup文件

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -eu
 rm -rf /home/ChatGPT-Telegram-Bot
 # 修改为克隆你的fork仓库，如果你有自己的fork仓库，请替换下面的URL
-git clone --recurse-submodules --depth 1 -b main --quiet https://github.com/yym68686/ChatGPT-Telegram-Bot.git
+git clone --recurse-submodules --depth 1 -b main --quiet https://github.com/Lapis0x0/ChatGPT-Telegram-Bot.git
 # 覆盖克隆的仓库中的文件，使用你挂载的自定义文件
 cp -f /home/bot.py /home/ChatGPT-Telegram-Bot/bot.py
 cp -rf /home/utils/* /home/ChatGPT-Telegram-Bot/utils/
